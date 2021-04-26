5fedbc56ff0133c0053383defdc28fd467db6eb5
liuhan <17391848619@163.com>
2021-04-26 20:03:27 +0800

    push commit

--- src/test/java/com/example/TestSelect.java
+++ src/test/java/com/example/TestSelect.java
@@ -31,4 +31,14 @@ public class TestSelect {
             System.out.println(maps);
         }
     }
+
+    @Test
+    public void TestThree(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        EmpDao dao = sqlSession.getMapper(EmpDao.class);
+        List<Map<String,Object>> mapList = dao.selectOne("ACCOUNTING","400");
+        for (Map<String,Object> maps : mapList){
+            System.out.println(maps);
+        }
+    }
 }
