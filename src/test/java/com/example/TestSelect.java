aa2e858dfe43d1b4b4ed56627bbae456569c9ef0
liuhan <17391848619@163.com>
2021-05-02 23:24:00 +0800

    push commit

--- src/test/java/com/example/TestSelect.java
+++ src/test/java/com/example/TestSelect.java
@@ -4,9 +4,13 @@ import com.example.dao.EmpDao;
 import com.example.entity.Dept;
 import com.example.entity.Emp;
 import com.example.untils.MyBatisUtils;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -33,13 +37,14 @@ public class TestSelect {
     }
 
     @Test
-    public void TestThree(){
-        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+    public void TestThree() throws IOException {
+        String config = "mybatis.xml";
+        InputStream in = Resources.getResourceAsStream(config);
+        SqlSession sqlSession = new SqlSessionFactoryBuilder().build(in).openSession();
         EmpDao dao = sqlSession.getMapper(EmpDao.class);
-        List<Map<String,Object>> mapList = dao.selectOne("ACCOUNTING","400");
-        for (Map<String,Object> maps : mapList){
-            System.out.println(maps);
-            System.out.println(maps);
-        }
+        dao.selectOne("ACCOUNTING","400");
     }
+
+
+
 }
